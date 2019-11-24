e30c15c9b0659bfb0cc2eb4120ae738ef7043700
Hideo Oshima <orimajp@gmail.com>
2019-11-25 01:13:12 +0900

    Github Pages公開設定変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,17 @@
 const pkg = require('./package')
 const path = require('path');
 
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/nuxt-traffic-information/'
+  }
+} : {}
+
 module.exports = {
   mode: 'spa',
   srcDir: 'app',
   generate: {dir: 'docs'},
+  ...routerBase,
 
   /*
   ** for IntelliJ IDEA / WebStorm
